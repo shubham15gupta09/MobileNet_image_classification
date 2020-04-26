c1361c4ac0470141fc4d36e6c417a1d52cea67a7
Shubham Gupta <shubham15gupta09@gmail.com>
2020-04-26 20:04:20 +0530

    cleanup function for deleting img added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var app = express();
+const fs = require("fs");
 var upload = require("express-fileupload");
 
 app.use(upload()); // configure middleware
@@ -7,7 +8,10 @@ app.use(upload()); // configure middleware
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/routes/index.html");
 });
-
+function clean_up(){
+  const path = '/images/image.jpg';
+  fs.unlink(path , ()=>{});
+}
 app.post("/upload", function (req, res) {
   console.log(req.files);
   if (req.files.upfile) {
@@ -16,7 +20,8 @@ app.post("/upload", function (req, res) {
     var uploadpath = __dirname + "/images/" + name;
     file.mv(uploadpath, function (err) {
       console.log("File Uploaded", name);
-      res.sendFile(__dirname+"/routes/mobilenet.html")
+      res.sendFile(__dirname+"/routes/mobilenet.html");
+      clean_up();
     });
   }
 });
@@ -25,7 +30,7 @@ app.get("/images/image.jpg",(req,res)=>{
   res.sendFile(__dirname+"/images/image.jpg")
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT , (req,res)=>{
   console.log("server started on port : 3000");
 });
